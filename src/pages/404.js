55338a28e5df7e8543d5e57463dd4a39ff932790
aabiru-sabeel <aabiru-sabeel@users.noreply.github.com>
2021-08-14 13:41:02 +0500

    Minor edits to files in src/components (About and Bio) and 404 in src/pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
     <main>
       <Helmet title={`404: Not Found | ${config.siteTitle}`} />
       <h1>404 Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist.</p>
     </main>
   </Layout>
 )
